0bc29b3ffdf8cf2a32c884b0f583e2edea118c6f
Isaac Saqib <isaac.saqib@stonybrook.edu>
2016-11-05 15:58:48 -0400

    adding in banner

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 	$("#nav").hide();
-
-
+	$("#free-shipping-div").slideDown();
+	
+	$("#free-shipping-x").on("click", function(){
+		$("#free-shipping-div").slideUp();
+	});
 
 	      $(".elevatezoom").elevateZoom();
 
